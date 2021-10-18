5f34ede19c33398fed262fee536547b241986662
Tudor Lechintan <lechintantudor02@gmail.com>
2021-10-18 12:03:53 +0300

    Update dependencies and remove criterion results

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -1,219 +1,37 @@
 use criterion::*;
-use ecs_bench_suite::*;
 
-fn bench_simple_insert(c: &mut Criterion) {
-    let mut group = c.benchmark_group("simple_insert");
-    group.bench_function("legion", |b| {
-        let mut bench = legion::simple_insert::Benchmark::new();
-        b.iter(move || bench.run());
-    });
-    group.bench_function("bevy", |b| {
-        let mut bench = bevy::simple_insert::Benchmark::new();
-        b.iter(move || bench.run());
-    });
-    group.bench_function("hecs", |b| {
-        let mut bench = hecs::simple_insert::Benchmark::new();
-        b.iter(move || bench.run());
-    });
-    group.bench_function("planck_ecs", |b| {
-        let mut bench = planck_ecs::simple_insert::Benchmark::new();
-        b.iter(move || bench.run());
-    });
-    group.bench_function("shipyard", |b| {
-        let mut bench = shipyard::simple_insert::Benchmark::new();
-        b.iter(move || bench.run());
-    });
-    group.bench_function("specs", |b| {
-        let mut bench = specs::simple_insert::Benchmark::new();
-        b.iter(move || bench.run());
-    });
+macro_rules! benchmark {
+    ($benchmark:ident; $($ecs:ident),+) => {
+        fn $benchmark(c: &mut Criterion) {
+            let mut group = c.benchmark_group(stringify!($benchmark));
+            $(
+                group.bench_function(stringify!($ecs), |b| {
+                    let mut bench = ecs_bench_suite::$ecs::$benchmark::Benchmark::new();
+                    b.iter(move || bench.run());
+                });
+            )+
+        }
+    };
 }
 
-fn bench_simple_iter(c: &mut Criterion) {
-    let mut group = c.benchmark_group("simple_iter");
-    group.bench_function("legion", |b| {
-        let mut bench = legion::simple_iter::Benchmark::new();
-        b.iter(move || bench.run());
-    });
-    group.bench_function("legion (packed)", |b| {
-        let mut bench = legion_packed::simple_iter::Benchmark::new();
-        b.iter(move || bench.run());
-    });
-    group.bench_function("bevy", |b| {
-        let mut bench = bevy::simple_iter::Benchmark::new();
-        b.iter(move || bench.run());
-    });
-    group.bench_function("hecs", |b| {
-        let mut bench = hecs::simple_iter::Benchmark::new();
-        b.iter(move || bench.run());
-    });
-    group.bench_function("planck_ecs", |b| {
-        let mut bench = planck_ecs::simple_iter::Benchmark::new();
-        b.iter(move || bench.run());
-    });
-    group.bench_function("shipyard", |b| {
-        let mut bench = shipyard::simple_iter::Benchmark::new();
-        b.iter(move || bench.run());
-    });
-    group.bench_function("specs", |b| {
-        let mut bench = specs::simple_iter::Benchmark::new();
-        b.iter(move || bench.run());
-    });
-}
-
-fn bench_frag_iter_bc(c: &mut Criterion) {
-    let mut group = c.benchmark_group("fragmented_iter");
-    group.bench_function("legion", |b| {
-        let mut bench = legion::frag_iter::Benchmark::new();
-        b.iter(move || bench.run());
-    });
-    group.bench_function("bevy", |b| {
-        let mut bench = bevy::frag_iter::Benchmark::new();
-        b.iter(move || bench.run());
-    });
-    group.bench_function("hecs", |b| {
-        let mut bench = hecs::frag_iter::Benchmark::new();
-        b.iter(move || bench.run());
-    });
-    group.bench_function("planck_ecs", |b| {
-        let mut bench = planck_ecs::frag_iter::Benchmark::new();
-        b.iter(move || bench.run());
-    });
-    group.bench_function("shipyard", |b| {
-        let mut bench = shipyard::frag_iter::Benchmark::new();
-        b.iter(move || bench.run());
-    });
-    group.bench_function("specs", |b| {
-        let mut bench = specs::frag_iter::Benchmark::new();
-        b.iter(move || bench.run());
-    });
-}
-
-fn bench_schedule(c: &mut Criterion) {
-    let mut group = c.benchmark_group("schedule");
-    group.bench_function("legion", |b| {
-        let mut bench = legion::schedule::Benchmark::new();
-        b.iter(move || bench.run());
-    });
-    group.bench_function("legion (packed)", |b| {
-        let mut bench = legion_packed::schedule::Benchmark::new();
-        b.iter(move || bench.run());
-    });
-    group.bench_function("bevy", |b| {
-        let mut bench = bevy::schedule::Benchmark::new();
-        b.iter(move || bench.run());
-    });
-    group.bench_function("planck_ecs", |b| {
-        let mut bench = planck_ecs::schedule::Benchmark::new();
-        b.iter(move || bench.run());
-    });
-    group.bench_function("shipyard", |b| {
-        let mut bench = shipyard::schedule::Benchmark::new();
-        b.iter(move || bench.run());
-    });
-    group.bench_function("specs", |b| {
-        let mut bench = specs::schedule::Benchmark::new();
-        b.iter(move || bench.run());
-    });
-}
-
-fn bench_heavy_compute(c: &mut Criterion) {
-    let mut group = c.benchmark_group("heavy_compute");
-    group.bench_function("legion", |b| {
-        let mut bench = legion::heavy_compute::Benchmark::new();
-        b.iter(move || bench.run());
-    });
-    group.bench_function("legion (packed)", |b| {
-        let mut bench = legion_packed::heavy_compute::Benchmark::new();
-        b.iter(move || bench.run());
-    });
-    group.bench_function("bevy", |b| {
-        let mut bench = bevy::heavy_compute::Benchmark::new();
-        b.iter(move || bench.run());
-    });
-    group.bench_function("hecs", |b| {
-        let mut bench = hecs::heavy_compute::Benchmark::new();
-        b.iter(move || bench.run());
-    });
-    group.bench_function("shipyard", |b| {
-        let mut bench = shipyard::heavy_compute::Benchmark::new();
-        b.iter(move || bench.run());
-    });
-    group.bench_function("specs", |b| {
-        let mut bench = specs::heavy_compute::Benchmark::new();
-        b.iter(move || bench.run());
-    });
-}
-
-fn bench_add_remove(c: &mut Criterion) {
-    let mut group = c.benchmark_group("add_remove_component");
-    group.bench_function("legion", |b| {
-        let mut bench = legion::add_remove::Benchmark::new();
-        b.iter(move || bench.run());
-    });
-    group.bench_function("hecs", |b| {
-        let mut bench = hecs::add_remove::Benchmark::new();
-        b.iter(move || bench.run());
-    });
-    group.bench_function("planck_ecs", |b| {
-        let mut bench = planck_ecs::add_remove::Benchmark::new();
-        b.iter(move || bench.run());
-    });
-    group.bench_function("shipyard", |b| {
-        let mut bench = shipyard::add_remove::Benchmark::new();
-        b.iter(move || bench.run());
-    });
-    group.bench_function("specs", |b| {
-        let mut bench = specs::add_remove::Benchmark::new();
-        b.iter(move || bench.run());
-    });
-    group.bench_function("bevy", |b| {
-        let mut bench = bevy::add_remove::Benchmark::new();
-        b.iter(move || bench.run());
-    });
-}
-
-fn bench_serialize_text(c: &mut Criterion) {
-    let mut group = c.benchmark_group("serialize_text");
-    group.bench_function("legion", |b| {
-        let mut bench = legion::serialize_text::Benchmark::new();
-        b.iter(move || bench.run());
-    });
-    group.bench_function("hecs", |b| {
-        let mut bench = hecs::serialize_text::Benchmark::new();
-        b.iter(move || bench.run());
-    });
-    // group.bench_function("bevy", |b| {
-    //     let mut bench = bevy::serialize_text::Benchmark::new();
-    //     b.iter(move || bench.run());
-    // });
-}
-
-fn bench_serialize_binary(c: &mut Criterion) {
-    let mut group = c.benchmark_group("serialize_binary");
-    group.bench_function("legion", |b| {
-        let mut bench = legion::serialize_binary::Benchmark::new();
-        b.iter(move || bench.run());
-    });
-    group.bench_function("hecs", |b| {
-        let mut bench = hecs::serialize_binary::Benchmark::new();
-        b.iter(move || bench.run());
-    });
-    // group.bench_function("bevy", |b| {
-    //     let mut bench = bevy::serialize_text::Benchmark::new();
-    //     b.iter(move || bench.run());
-    // });
-}
+benchmark!(simple_insert; bevy, hecs, legion, planck_ecs, shipyard, specs);
+benchmark!(simple_iter; bevy, hecs, legion, legion_packed, planck_ecs, shipyard, specs);
+benchmark!(frag_iter; bevy, hecs, legion, planck_ecs, shipyard, specs);
+benchmark!(schedule; bevy, legion, legion_packed, planck_ecs, shipyard, specs);
+benchmark!(heavy_compute; bevy, legion, legion_packed, shipyard, specs);
+benchmark!(add_remove; bevy, hecs, legion, planck_ecs, shipyard, specs);
+benchmark!(serialize_text; hecs);
+benchmark!(serialize_binary; hecs);
 
 criterion_group!(
     benchmarks,
-    bench_simple_insert,
-    bench_simple_iter,
-    bench_frag_iter_bc,
-    bench_schedule,
-    bench_heavy_compute,
-    bench_add_remove,
-    bench_serialize_text,
-    bench_serialize_binary,
+    simple_insert,
+    simple_iter,
+    frag_iter,
+    schedule,
+    heavy_compute,
+    add_remove,
+    serialize_text,
+    serialize_binary,
 );
 criterion_main!(benchmarks);
--- readme.md
+++ readme.md
@@ -2,7 +2,6 @@
 
 A suite of benchmarks designed to test and compare Rust ECS library performance across a variety of challenging circumstances.
 
-The full benchmark report is available [here](https://rust-gamedev.github.io/ecs_bench_suite/target/criterion/report/index.html).
 
 ## The Benchmarks
 
@@ -12,8 +11,6 @@ This benchmark is designed to test the base cost of constructing entities and mo
 
 Inserts 10,000 entities, each with 4 components: `Transform(mat4x4)`, `Position(vec3)`, `Rotation(vec3)` and `Velocity(vec3)`.
 
-![](./target/criterion/simple_insert/report/violin.svg)
-
 ### Simple Iter
 
 This benchmark is designed to test the core overheads involved in component iteration in best-case conditions. The iteration should occur on a single CPU core.
@@ -22,8 +19,6 @@ Dataset: 10,000 entities, each with 4 components: `Transform(mat4x4)`, `Position
 
 Test: Iterate through all entities with `Position` and `Velocity`, and add velocity onto position.
 
-![](./target/criterion/simple_iter/report/violin.svg)
-
 ### Fragmented Iter
 
 This benchmark is designed to test how the ECS handles iteration through a fragmented dataset. The iteration should occur on a single CPU core.
@@ -31,8 +26,6 @@ This benchmark is designed to test how the ECS handles iteration through a fragm
 Dataset: 26 component types (`A(f32)` through `Z(f32)`), each with 20 entities plus a `Data(f32)` component.
 
 Test: Iterate through all entities with a `Data` component and double its value.
-
-![](./target/criterion/fragmented_iter/report/violin.svg)
 
 ### System Scheduling
 
@@ -53,8 +46,6 @@ Three systems accessing the following components mutably, where each system swap
 * `(C, D)`
 * `(C, E)`
 
-![](./target/criterion/schedule/report/violin.svg)
-
 ### Heavy Compute
 
 This benchmark is designed to test the ECS's ability to scale when it is allowed to run a system over multiple CPU cores. This is primarily an inner-parallelism test.
@@ -62,8 +53,6 @@ This benchmark is designed to test the ECS's ability to scale when it is allowed
 Dataset: 1,000 entities with a `mat4x4` component.
 
 Test: Iterate through all `mat4x4` components, and invert the matrix 100 times.
-
-![](./target/criterion/heavy_compute/report/violin.svg)
 
 ### Add/Remove Component
 
@@ -73,8 +62,6 @@ Dataset: 10,000 entities with a single `A` component.
 
 Test: Iterate through all entities, adding a `B` component. Then iterate through all entities again, removing their `B` component.
 
-![](./target/criterion/add_remove_component/report/violin.svg)
-
 ### Serialize
 
 This benchmark is designed to test how quickly the ECS and serialize and deserialize its entities in both text (RON) and binary (bincode) formats.
@@ -82,6 +69,3 @@ This benchmark is designed to test how quickly the ECS and serialize and deseria
 Dataset: 1000 entities with `Transform(mat4x4)`, `Position(vec3)`, `Rotation(vec3)` and `Velocity(vec3)` components.
 
 Test: Serialize all entities to RON and bincode formats in-memory. Then deserialize back into the ECS. The RON and bincode formats should be separate benchmark tests.
-
-![](./target/criterion/serialize_text/report/violin.svg)
-![](./target/criterion/serialize_binary/report/violin.svg)
