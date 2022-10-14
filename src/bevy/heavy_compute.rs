e0f30deb4ba59951ca2ab836d9c1db8af6271e46
Tudor Lechintan <lechintantudor02@gmail.com>
2022-10-14 20:10:49 +0300

    Update criterion ad fix bevy heavy compute benchmark

--- src/bevy/heavy_compute.rs
+++ src/bevy/heavy_compute.rs
@@ -1,4 +1,5 @@
 use bevy_ecs::prelude::*;
+use bevy_tasks::{ComputeTaskPool, TaskPool};
 use cgmath::*;
 
 #[derive(Clone, Copy, Component)]
@@ -27,6 +28,8 @@ impl Benchmark {
                 Velocity(Vector3::unit_x()),
             )
         }));
+
+        ComputeTaskPool::init(TaskPool::new);
 
         Self(world)
     }
